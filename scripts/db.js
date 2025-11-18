d09cc3f97dea5d1c85d9b3dd9dd835523e2f4091
SOURCE-CODE <143252411+ADERICHTI@users.noreply.github.com>
2025-11-18 03:24:07 +0100

    Add files via upload

---
+++ scripts/db.js
@@ -0,0 +1,120 @@
+const EME127QuestionsData = {
+    "EME 127 Test": {
+    "required": true,
+    "questions": [
+        {
+            "No": 1,
+            "question": "Purchasing functions involve all activities undertaken by a business",
+            "options": [
+                "I. To acquire the necessary goods, and services from external sources to operate and produce its products",
+                "II. To acquire the necessary goods and materials from external sources to produce its products",
+                "III. To acquire the necessary goods, services and materials from external sources to operate and produce its products",
+                "IV. To acquire the necessary goods, and services from suppliers to produce its products"
+            ],
+            "answers": ["III"],
+        },
+        {
+            "No": 2,
+            "question": "One of the options contains an element which is not a core activity of purchasing function. What is it?",
+            "options": [
+                "I. Need identification, market research, invoice verification and negotiation.",
+                "II. Supplier selection, logistics, order-placement and follow-up.",
+                "III. Invoice verification, relationship management, supplier selection and market research.",
+                "IV. Order-placement, follow-up and negotiation"
+            ],
+            "answers": ["II"],
+        },
+        {
+            "No": 3,
+            "question": "Importance of contracts include the following except",
+            "options": [
+                "I. Provision of a legal framework for transaction to protect buyer and seller.",
+                "II. Reduce ambiguity by providing clear expectations for both buyer and seller.",
+                "III. Buyers are able to pay for the time spent and materials used, which is often used when the project scope is not clearly defined.",
+                "IV. Risk management is made possible by addressing potential issues like cost increases or delays upfront."
+            ],
+            "answers": ["III"],
+        },
+        {
+            "No": 4,
+            "question": "Purchase order is",
+            "options": [
+                "I. A legal document or request within a company to initiate a purchase",
+                "II. A process that includes strategic sourcing, procurement, and payment activities",
+                "III. A formal legal document from a buyer to a supplier, confirming the intent to procure goods or services",
+                "IV. The broader process of purchasing goods and services involving sourcing, negotiation and purchasing."
+            ],
+            "answers": ["III"],
+        },
+        {
+            "No": 5,
+            "question": "Source-to-pay (S2P) is",
+            "options": [
+                "I. A channel that shows goods or services have been delivered by the supplier crucial for 2-way matching.",
+                "II. A more comprehensive process which includes strategic sourcing, procurement and payment activities.",
+                "III. Placing orders, tracking deliveries and ensuring timely arrival of goods.",
+                "IV. An integrated system connecting the entire purchasing process from requisition to invoice payment."
+            ],
+            "answers": ["II"],
+        },
+        {
+            "No": 6,
+            "question": "Essential elements for a valid contract include",
+            "options": [
+                "I. Legal capacity, common purpose, legality of purpose, offer etc.",
+                "II. Offer, acceptance, legislation, common purpose etc.",
+                "III. Offer, consideration, legal capacity, acceptance etc.",
+                "IV. Mutual consent, intention to create legal relations, legislation, ethical consideration etc."
+            ],
+            "answers": ["III"],
+        },
+        {
+            "No": 7,
+            "question": "To establish a supply network that supports organisational goals, the five basic supplier's selection activities include",
+            "options": [
+                "I. Supplier identification, supplier evaluation, supplier screening, negotiation and contracting, and supplier onboarding.",
+                "II. Supplier pre-qualification, supplier contractual purpose, supplier screening, contracting and supplier onboarding.",
+                "III. supplier contractual purpose, supplier identification, supplier screening, supplier onboarding and negotiation.",
+                "IV. Supplier identification, supplier evaluation, supplier screening, supplier onboarding and supplier contractual purpose."
+            ],
+            "answers": ["I"],
+        },
+        {
+            "No": 8,
+            "question": "Procure-to-pay (P2P) means",
+            "options": [
+                "I. Placing orders, tracking deliveries and ensuring timely arrival of goods.",
+                "II. A channel that shows goods or services have been delivered by the supplier crucial for 2-way matching.",
+                "III. An integrated system connecting the entire purchasing process from requisition to invoice payment.",
+                "IV. A more comprehensive process which includes strategic sourcing, procurement and payment activities."
+            ],
+            "answers": ["IV"],
+        },
+        {
+            "No": 9,
+            "question": "Purchase requisition means",
+            "options": [
+                "I. A formal legal document from a buyer to a supplier, confirming the intent to procure goods or services",
+                "II. The broader process of purchasing goods and services involving sourcing, negotiation and purchasing.",
+                "III. Placing orders, tracking deliveries and ensuring timely arrival of goods.",
+                "IV. An internal document or request within a company to initiate a purchase."
+            ],
+            "answers": ["IV"],
+        },
+        {
+            "No": 10,
+            "question": "2/10 Net 30 as a term of payment for goods means",
+            "options": [
+                "I. A buyer will receive 2% discount if he can pay for the goods 10 times within 30 days.",
+                "II. A buyer will receive 10% discount if he can pay for the goods 2 times within 30 days.",
+                "III. A buyer will enjoy 2% discount for payment in 10 days, with the balance due in 30 days",
+                "IV. A buyer will enjoy 10% 2 times within 30 days if he can pay without delay."
+            ],
+            "answers": ["III"],
+        }
+    ]
+}
+
+  }
+
+const questionsData = {...EME127QuestionsData};
