73a650bbf64f6d28d860fd761eacc47bd7f55d0e
SOURCE-CODE <143252411+ADERICHTI@users.noreply.github.com>
2025-11-20 07:02:58 +0100

    Add files via upload

--- scripts/db.js
+++ scripts/db.js
@@ -4,126 +4,6 @@ const EME127QuestionsData = {
         "questions": [
             {
                 "No": 1,
-                "question": "Spending less and increasing the bottom line is the goal for",
-                "options": [
-                    "I. Most purchase teams",
-                    "II. Most procurement teams",
-                    "III. Most Purchase and supply teams",
-                    "IV. Most purchase or procurement teams"
-                ],
-                "answers": ["II"],
-                
-            },
-            {
-                "No": 2,
-                "question": "The five basic supplier's selection activities to establish a supply network that supports organisational goals include",
-                "options": [
-                    "I. Supplier pre-qualification, supplier contractual purpose, supplier screening, contracting and supplier onboarding.",
-                    "II. supplier contractual purpose, supplier identification, supplier screening, supplier onboarding and negotiation.",
-                    "III. Supplier identification, supplier evaluation, supplier screening, negotiation and contracting, and supplier onboarding.",
-                    "IV. Supplier identification, supplier evaluation, supplier screening, supplier onboarding and supplier contractual purpose."
-                ],
-                "answers": ["III"],
-                
-            },
-            {
-                "No": 3,
-                "question": "One of the following is not an importance of contracts",
-                "options": [
-                    "I. Provision of a legal framework for transaction to protect buyer and seller.",
-                    "II. Reduce ambiguity by providing clear expectations for both buyer and seller.",
-                    "III. Risk management is made possible by addressing potential issues like cost increases or delays upfront.",
-                    "IV. Buyers are able to pay for the time spent and materials used, which is often used when the project scope is not clearly defined."
-                ],
-                "answers": ["IV"],
-                
-            },
-            {
-                "No": 4,
-                "question": "Purchasing functions involve all activities undertaken by a business",
-                "options": [
-                    "I. To acquire the necessary goods, and services from suppliers to produce its products",
-                    "II. To acquire the necessary goods and materials from external sources to produce its products",
-                    "III. To acquire the necessary goods, and services from external sources to operate and produce its products",
-                    "IV. To acquire the necessary goods, services and materials from external sources to operate and produce its products."
-                ],
-                "answers": ["IV"],
-                
-            },
-            {
-                "No": 5,
-                "question": "The following are factors central to the sourcing process except",
-                "options": [
-                    "I. Cost structure",
-                    "II. Profit margins",
-                    "III. Budget size",
-                    "IV. Competitiveness"
-                ],
-                "answers": ["III"],
-                
-            },
-            {
-                "No": 6,
-                "question": "Source-to-pay (S2P) means",
-                "options": [
-                    "I. A more comprehensive process which includes strategic sourcing, procurement and payment activities.",
-                    "II. A channel that shows goods or services have been delivered by the supplier crucial for 2-way matching.",
-                    "III. Placing orders, tracking deliveries and ensuring timely arrival of goods.",
-                    "IV. An integrated system connecting the entire purchasing process from requisition to invoice payment."
-                ],
-                "answers": ["I"],
-                
-            },
-            {
-                "No": 7,
-                "question": "The sourcing process is about",
-                "options": [
-                    "I. Collecting data on good quality sources of goods and services",
-                    "II. Negotiating contracts",
-                    "III. Considering outsourcing for goods",
-                    "IV. Considering potential profit."
-                ],
-                "answers": ["IV"],
-                
-            },
-            {
-                "No": 8,
-                "question": "Essential elements for a valid contract include",
-                "options": [
-                    "I. Legal capacity, common purpose, legality of purpose, offer etc.",
-                    "II. Offer, acceptance, legislation, common purpose etc.",
-                    "III. Offer, consideration, legal capacity, acceptance etc.",
-                    "IV. Mutual consent, intention to create legal relations, legislation, ethical consideration etc."
-                ],
-                "answers": ["III"],
-                
-            },
-            {
-                "No": 9,
-                "question": "Which of the following is not a core purchasing function activity?",
-                "options": [
-                    "I. Need identification, market research, invoice verification and negotiation.",
-                    "II. Invoice verification, relationship management, supplier selection and market research.",
-                    "III. Supplier selection, logistics, order-placement and follow-up.",
-                    "IV. Order-placement, follow-up and negotiation"
-                ],
-                "answers": ["III"],
-                
-            },
-            {
-                "No": 10,
-                "question": "Procure-to-pay (P2P) means",
-                "options": [
-                    "I. Placing orders, tracking deliveries and ensuring timely arrival of goods.",
-                    "II. A channel that shows goods or services have been delivered by the supplier crucial for 2-way matching.",
-                    "III. An integrated system connecting the entire purchasing process from requisition to invoice payment.",
-                    "IV. A more comprehensive process which includes strategic sourcing, procurement and payment activities."
-                ],
-                "answers": ["IV"],
-                
-            },
-            {
-                "No": 11,
                 "question": "Sourcing Models include but not",
                 "options": [
                     "I. performance-based/managed services model",
@@ -131,35 +11,32 @@ const EME127QuestionsData = {
                     "III. approved provider model",
                     "IV. basic provider model"
                 ],
-                "answers": ["II"],
-                
+                "answers": ["II"]
             },
             {
-                "No": 12,
-                "question": "Purchase requisition means",
+                "No": 2,
+                "question": "The following are factors central to the sourcing process except",
                 "options": [
-                    "I. A formal legal document from a buyer to a supplier, confirming the intent to procure goods or services",
-                    "II. The broader process of purchasing goods and services involving sourcing, negotiation and purchasing.",
-                    "III. Placing orders, tracking deliveries and ensuring timely arrival of goods.",
-                    "IV. An internal document or request within a company to initiate a purchase."
+                    "I. Cost structure",
+                    "II. Profit margins",
+                    "III. Budget size",
+                    "IV. Competitiveness"
                 ],
-                "answers": ["IV"],
-                
+                "answers": ["III"]
             },
             {
-                "No": 13,
-                "question": "2/10 Net 30 as a term of payment for goods means",
+                "No": 3,
+                "question": "The five basic supplier's selection activities to establish a supply network that supports organisational goals include",
                 "options": [
-                    "I. A buyer will receive 2% discount if he can pay for the goods 10 times within 30 days.",
-                    "II. A buyer will enjoy 2% discount for payment in 10 days, with the balance due in 30 days.",
-                    "III. A buyer will receive 10% discount if he can pay for the goods 2 times within 30 days.",
-                    "IV. A buyer will enjoy 10% 2 times within 30 days if he can pay without delay."
+                    "I. Supplier pre-qualification, supplier contractual purpose, supplier screening, contracting and supplier onboarding.",
+                    "II. supplier contractual purpose, supplier identification, supplier screening, supplier onboarding and negotiation.",
+                    "III. Supplier identification, supplier evaluation, supplier screening, negotiation and contracting, and supplier onboarding.",
+                    "IV. Supplier identification, supplier evaluation, supplier screening, supplier onboarding and supplier contractual purpose."
                 ],
-                "answers": ["II"],
-                
+                "answers": ["III"]
             },
             {
-                "No": 14,
+                "No": 4,
                 "question": "Purchase order is",
                 "options": [
                     "I. A formal legal document or request within a company to initiate a purchase",
@@ -167,11 +44,98 @@ const EME127QuestionsData = {
                     "III. The broader process of purchasing goods and services involving sourcing, negotiation and purchasing.",
                     "IV. A formal legal document from a buyer to a supplier, confirming the intent to procure goods or services."
                 ],
-                "answers": ["IV"],
-                
+                "answers": ["IV"]
             },
             {
-                "No": 15,
+                "No": 5,
+                "question": "The sourcing process is about",
+                "options": [
+                    "I. Collecting data on good quality sources of goods and services",
+                    "II. Negotiating contracts",
+                    "III. Considering outsourcing for goods",
+                    "IV. Considering potential profit."
+                ],
+                "answers": ["IV"]
+            },
+            {
+                "No": 6,
+                "question": "Which of the following is not a core purchasing function activity?",
+                "options": [
+                    "I. Need identification, market research, invoice verification and negotiation.",
+                    "II. Invoice verification, relationship management, supplier selection and market research.",
+                    "III. Supplier selection, logistics, order-placement and follow-up.",
+                    "IV. Order-placement, follow-up and negotiation"
+                ],
+                "answers": ["III"]
+            },
+            {
+                "No": 7,
+                "question": "2/10 Net 30 as a term of payment for goods means",
+                "options": [
+                    "I. A buyer will receive 2% discount if he can pay for the goods 10 times within 30 days.",
+                    "II. A buyer will enjoy 2% discount for payment in 10 days, with the balance due in 30 days.",
+                    "III. A buyer will receive 10% discount if he can pay for the goods 2 times within 30 days.",
+                    "IV. A buyer will enjoy 10% 2 times within 30 days if he can pay without delay."
+                ],
+                "answers": ["II"]
+            },
+            {
+                "No": 8,
+                "question": "One of the following is not an importance of contracts",
+                "options": [
+                    "I. Provision of a legal framework for transaction to protect buyer and seller.",
+                    "II. Reduce ambiguity by providing clear expectations for both buyer and seller.",
+                    "III. Risk management is made possible by addressing potential issues like cost increases or delays upfront.",
+                    "IV. Buyers are able to pay for the time spent and materials used, which is often used when the project scope is not clearly defined."
+                ],
+                "answers": ["IV"]
+            },
+            {
+                "No": 9,
+                "question": "Source-to-pay (S2P) means",
+                "options": [
+                    "I. A more comprehensive process which includes strategic sourcing, procurement and payment activities.",
+                    "II. A channel that shows goods or services have been delivered by the supplier crucial for 2-way matching.",
+                    "III. Placing orders, tracking deliveries and ensuring timely arrival of goods.",
+                    "IV. An integrated system connecting the entire purchasing process from requisition to invoice payment."
+                ],
+                "answers": ["I"]
+            },
+            {
+                "No": 10,
+                "question": "Essential elements for a valid contract include",
+                "options": [
+                    "I. Legal capacity, common purpose, legality of purpose, offer etc.",
+                    "II. Offer, acceptance, legislation, common purpose etc.",
+                    "III. Offer, consideration, legal capacity, acceptance etc.",
+                    "IV. Mutual consent, intention to create legal relations, legislation, ethical consideration etc."
+                ],
+                "answers": ["III"]
+            },
+            {
+                "No": 11,
+                "question": "Purchase requisition means",
+                "options": [
+                    "I. A formal legal document from a buyer to a supplier, confirming the intent to procure goods or services",
+                    "II. The broader process of purchasing goods and services involving sourcing, negotiation and purchasing.",
+                    "III. Placing orders, tracking deliveries and ensuring timely arrival of goods.",
+                    "IV. An internal document or request within a company to initiate a purchase."
+                ],
+                "answers": ["IV"]
+            },
+            {
+                "No": 12,
+                "question": "Spending less and increasing the bottom line is the goal for",
+                "options": [
+                    "I. Most purchase teams",
+                    "II. Most procurement teams",
+                    "III. Most Purchase and supply teams",
+                    "IV. Most purchase or procurement teams"
+                ],
+                "answers": ["II"]
+            },
+            {
+                "No": 13,
                 "question": "Strict liability means",
                 "options": [
                     "I. the buyer's intent or knowledge of the infringement is not necessary for the patent holder to seek monetary damages.",
@@ -179,8 +143,29 @@ const EME127QuestionsData = {
                     "III. the buyer's intent or knowledge of the infringement is enough for the patent holder to collect monetary damages.",
                     "IV. the buyer's intent or knowledge of the infringement is not enough for the patent holder to collect monetary damages."
                 ],
-                "answers": ["I"],
-                
+                "answers": ["I"]
+            },
+            {
+                "No": 14,
+                "question": "Procure-to-pay (P2P) means",
+                "options": [
+                    "I. Placing orders, tracking deliveries and ensuring timely arrival of goods.",
+                    "II. A channel that shows goods or services have been delivered by the supplier crucial for 2-way matching.",
+                    "III. An integrated system connecting the entire purchasing process from requisition to invoice payment.",
+                    "IV. A more comprehensive process which includes strategic sourcing, procurement and payment activities."
+                ],
+                "answers": ["IV"]
+            },
+            {
+                "No": 15,
+                "question": "Purchasing functions involve all activities undertaken by a business",
+                "options": [
+                    "I. To acquire the necessary goods, and services from suppliers to produce its products",
+                    "II. To acquire the necessary goods and materials from external sources to produce its products",
+                    "III. To acquire the necessary goods, and services from external sources to operate and produce its products",
+                    "IV. To acquire the necessary goods, services and materials from external sources to operate and produce its products."
+                ],
+                "answers": ["IV"]
             }
         ]
     }
