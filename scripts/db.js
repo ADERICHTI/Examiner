e6fd2f60024020cce5830569aaa1e2bb9ea56ac2
SOURCE-CODE <143252411+ADERICHTI@users.noreply.github.com>
2025-12-24 05:41:22 +0100

    Update db.js

--- scripts/db.js
+++ scripts/db.js
@@ -4,14 +4,14 @@ const EME401QuestionsData = {
         "questions": [
             {
                 "No": 1,
-                "question": "In the course of planning and administering educational services to the different group",
+                "question": "Socio-political context of educational planning and administration tries to",
                 "options": [
-                    "I. The pluralistic nature of Nigeria society ought to determine the type of education for the various groups.",
-                    "II. It is important to consider environmental background of the various groups.",
-                    "III. We must ensure that no group moves far away from the shared or common value.",
-                    "IV. The heterogenous nature of Nigeria society ought to influence the type of education requires for the various groups."
+                    "I. Accentuate that knowledge of social studies and political science is essential for educational planners and administrations.",
+                    "II. Explain the relationship between social-political development of a nation and educational planning and administration.",
+                    "III. Emphasize that social and political activities need to be studied for effective planning and administration of education.",
+                    "IV. To describe how education can be provided putting into consideration the various factors that could influence its success or failure."
                 ],
-                "answers": ["II"]
+                "answers": ["IV"]
             },
             {
                 "No": 2,
@@ -26,50 +26,17 @@ const EME401QuestionsData = {
             },
             {
                 "No": 3,
-                "question": "Select a sentence which is not correct",
+                "question": "In the course of planning and administering educational services to the different group",
                 "options": [
-                    "I. The presence of school in the community can expose the community to social amenities.",
-                    "II. Members of the community where a school is located can provide security for the facilities in that school.",
-                    "III. Good school community relations are very essential to be a successful manager.",
-                    "IV. Members of the community where a school is located must provide financial support and physical labour for the development of the school."
+                    "I. The pluralistic nature of Nigeria society ought to determine the type of education for the various groups.",
+                    "II. It is important to consider environmental background of the various groups.",
+                    "III. We must ensure that no group moves far away from the shared or common value.",
+                    "IV. The heterogenous nature of Nigeria society ought to influence the type of education requires for the various groups."
                 ],
-                "answers": ["IV"]
+                "answers": ["II"]
             },
             {
                 "No": 4,
-                "question": "Factors that influence educational planning and administration does not include",
-                "options": [
-                    "I. Technology and politics",
-                    "II. Religion and culture",
-                    "III. Politics and economy",
-                    "IV. Science and technology"
-                ],
-                "answers": ["IV"]
-            },
-            {
-                "No": 5,
-                "question": "Socio-political context of educational planning and administration tries to",
-                "options": [
-                    "I. Accentuate that knowledge of social studies and political science is essential for educational planners and administrations.",
-                    "II. Explain the relationship between social-political development of a nation and educational planning and administration.",
-                    "III. Emphasize that social and political activities need to be studied for effective planning and administration of education.",
-                    "IV. To describe how education can be provided putting into consideration the various factors that could influence its success or failure."
-                ],
-                "answers": ["IV"]
-            },
-            {
-                "No": 6,
-                "question": "In Northern Nigeria, Islamic studies",
-                "options": [
-                    "I. May and may not be included as a subject of study since Nigeria is a secular state.",
-                    "II. Should be included as an elective subject of study.",
-                    "III. Should be included as a compulsory subject of study.",
-                    "IV. May be included as a compulsory subject of study."
-                ],
-                "answers": ["III"]
-            },
-            {
-                "No": 7,
                 "question": "The federal government of Nigeria is __________ to change our academic policies",
                 "options": [
                     "I. Education",
@@ -80,7 +47,7 @@ const EME401QuestionsData = {
                 "answers": ["IV"]
             },
             {
-                "No": 8,
+                "No": 5,
                 "question": "The current school __________ has been informed the Federal government is planning to change the schooling system in Nigeria",
                 "options": [
                     "I. Education",
@@ -91,7 +58,7 @@ const EME401QuestionsData = {
                 "answers": ["II"]
             },
             {
-                "No": 9,
+                "No": 6,
                 "question": "The essence is to improve the __________ we get",
                 "options": [
                     "I. Education",
@@ -102,7 +69,18 @@ const EME401QuestionsData = {
                 "answers": ["I"]
             },
             {
-                "No": 10,
+                "No": 7,
+                "question": "Factors that influence educational planning and administration does not include",
+                "options": [
+                    "I. Technology and politics",
+                    "II. Religion and culture",
+                    "III. Politics and economy",
+                    "IV. Science and technology"
+                ],
+                "answers": ["IV"]
+            },
+            {
+                "No": 8,
                 "question": "One of the following is not a characteristic of education",
                 "options": [
                     "I. There is a process of transmission.",
@@ -113,7 +91,7 @@ const EME401QuestionsData = {
                 "answers": ["II"]
             },
             {
-                "No": 11,
+                "No": 9,
                 "question": "The school National development plan of Nigeria puts the national objectives of the country as the buildings of",
                 "options": [
                     "I. A just and democratic society.",
@@ -124,18 +102,7 @@ const EME401QuestionsData = {
                 "answers": ["III"]
             },
             {
-                "No": 12,
-                "question": "The Federal government has put in place quota admission policy",
-                "options": [
-                    "I. To reduce the gap in North-south level of education department.",
-                    "II. To reduce the gap in North-west level of education department.",
-                    "III. To reduce the gap in South-east level of education department.",
-                    "IV. To reduce the gap in South-west level of education department."
-                ],
-                "answers": ["I"]
-            },
-            {
-                "No": 13,
+                "No": 10,
                 "question": "The prescription of the 1882 Education ordinance include",
                 "options": [
                     "I. Creation of Education Board.",
@@ -146,40 +113,18 @@ const EME401QuestionsData = {
                 "answers": ["III"]
             },
             {
-                "No": 14,
-                "question": "One of the following is not true of the 1887 Education Ordinance",
+                "No": 11,
+                "question": "The Federal government has put in place quota admission policy",
                 "options": [
-                    "I. It stipulates principal's power to grant scholarship for secondary education.",
-                    "II. It is a purely Nigerian Education ordinance to be enacted.",
-                    "III. It came as a result of the separation of Lagos Colony from the Gold coast.",
-                    "IV. It created an Education Board."
+                    "I. To reduce the gap in North-south level of education department.",
+                    "II. To reduce the gap in North-west level of education department.",
+                    "III. To reduce the gap in South-east level of education department.",
+                    "IV. To reduce the gap in South-west level of education department."
                 ],
                 "answers": ["I"]
             },
             {
-                "No": 15,
-                "question": "The percentage recommended for grant-in-aid offered to efficiency of the feeding staff is",
-                "options": [
-                    "I. 40%",
-                    "II. 30%",
-                    "III. 20%",
-                    "IV. 10%"
-                ],
-                "answers": ["III"]
-            },
-            {
-                "No": 16,
-                "question": "One of the recommendations of the 1926 Education ordinance was",
-                "options": [
-                    "I. Creation of Education Board and giving board power to grant scholarship.",
-                    "II. Award of grants for organization and discipline with special grants for schools which obtained a high percentage of passes and those attained high standard of general excellence.",
-                    "III. Organize periodical examination and enhance tone of the school, discipline organization and moral institution.",
-                    "IV. Authorizing the closure of any school which was conducted in a way that was in conflict with the interest of the people or the host community."
-                ],
-                "answers": ["IV"]
-            },
-            {
-                "No": 17,
+                "No": 12,
                 "question": "The 1990 World Conference on Education in Jomtien, Thailand gave all signatory countries to ensure the provision of primary education for all by",
                 "options": [
                     "I. The year 2000.",
@@ -190,7 +135,29 @@ const EME401QuestionsData = {
                 "answers": ["I"]
             },
             {
-                "No": 18,
+                "No": 13,
+                "question": "The 1926 Education ordinance was primarily focused on",
+                "options": [
+                    "I. Creation of Education Board and giving board power to grant scholarship.",
+                    "II. Award of grants for organization and discipline with special grants for schools which obtained a high percentage of passes and those attained high standard of general excellence.",
+                    "III. Organize periodical examination and enhance tone of the school, discipline organization and moral institution.",
+                    "IV. Authorizing the closure of any school which was conducted in a way that was in conflict with the interest of the people or the host community."
+                ],
+                "answers": ["IV"]
+            },
+            {
+                "No": 14,
+                "question": "In Northern Nigeria, Islamic studies",
+                "options": [
+                    "I. May and may not be included as a subject of study since Nigeria is a secular state.",
+                    "II. Should be included as an elective subject of study.",
+                    "III. Should be included as a compulsory subject of study.",
+                    "IV. May be included as a compulsory subject of study."
+                ],
+                "answers": ["III"]
+            },
+            {
+                "No": 15,
                 "question": "Which of the following is true of pressure groups",
                 "options": [
                     "I. They promote interpersonal relationship among members and management or government.",
@@ -201,7 +168,7 @@ const EME401QuestionsData = {
                 "answers": ["I"]
             },
             {
-                "No": 19,
+                "No": 16,
                 "question": "Methods of manpower requirement approach of educational planning does not include",
                 "options": [
                     "I. Harbison's Rule of Thumb.",
@@ -212,19 +179,49 @@ const EME401QuestionsData = {
                 "answers": ["III"]
             },
             {
-                "No": 20,
-                "question": "Economic approaches to education include",
+                "No": 17,
+                "question": "The prescription of the 1882 Education ordinance include",
                 "options": [
-                    "I. Social cost consideration, manpower forecast and investment efficiency.",
-                    "II. Manpower requirement, social demand and investment efficiency.",
-                    "III. Investment effectiveness, social requirement and manpower.",
-                    "IV. Social demand, manpower requirement and investment-consumption analysis."
+                    "I. Creation of Education Board.",
+                    "II. Stipulation of rates and conditions for the award of quantity.",
+                    "III. A capitation grants in proportion of the average attendance at school.",
+                    "IV. Stipulation of standard of examination."
                 ],
-                "answers": ["II"]
+                "answers": ["III"]
+            },
+            {
+                "No": 18,
+                "question": "The percentage recommended for grant-in-aid offered to efficiency of the feeding staff is",
+                "options": [
+                    "I. 40%",
+                    "II. 30%",
+                    "III. 20%",
+                    "IV. 10%"
+                ],
+                "answers": ["III"]
+            },
+            {
+                "No": 19,
+                "question": "The prescription of the 1887 Education Ordinance included",
+                "options": [
+                    "I. It stipulates principal's power to grant scholarship for secondary education.",
+                    "II. It is a purely Nigerian Education ordinance to be enacted.",
+                    "III. It came as a result of the separation of Lagos Colony from the Gold coast.",
+                    "IV. It created an Education Board."
+                ],
+                "answers": ["IV"]
+            },
+            {
+                "No": 20,
+                "question": "One of the following is not true of the 1887 Education Ordinance",
+                "options": [
+                    "I. It stipulates principal's power to grant scholarship for secondary education.",
+                    "II. It is a purely Nigerian Education ordinance to be enacted.",
+                    "III. It came as a result of the separation of Lagos Colony from the Gold coast.",
+                    "IV. It created an Education Board."
+                ],
+                "answers": ["I"]
             }
         ]
     }
 };
-
-const questionsData = {...EME401QuestionsData};
-
